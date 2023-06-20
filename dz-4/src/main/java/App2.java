d9d37751bc54cd3416c84d78fa3dcff0994ea0ed
annie23 <annie.skrypka@gmail.com>
2023-06-20 20:56:00 +0300

    Changed indentations and ouptut format in  App1 and App2

--- dz-4/src/main/java/App2.java
+++ dz-4/src/main/java/App2.java
@@ -2,7 +2,7 @@ public class App2 {
     int intField = 23;
 
     int getInt() {
-        System.out.println(intField);
+        System.out.println("---" + intField + "---");
         return intField;
 
     }
