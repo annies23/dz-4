8d0290a1992fc81f81b18b6a4b7aca57815e969b
annie23 <annie.skrypka@gmail.com>
2023-06-20 21:01:11 +0300

    Deleted comment in App1

--- dz-4/src/main/java/App1.java
+++ dz-4/src/main/java/App1.java
@@ -2,7 +2,7 @@ public class App1 {
     public static void main(String[] argv){
         App2 example = new App2();
         System.out.println("~~~" + example.getInt() + "~~~");
-        //System.out.println(example.intField);
     }
+
 
 }
